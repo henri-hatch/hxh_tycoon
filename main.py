34ca60862229dc4f798abc08267f14caeb8e205c
henri <henrihatch@gmail.com>
2023-02-01 23:55:23 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Hunter x Hunter Tycoon
+# Configure your own "nen ability" which is pretty much just the title + brief description and stats
+# Train different stats to grow stronger
+# Train the different nen trainings (such as Gyo) by increasing stats
+# Do combat to train stats by comparing randomly generated stats based on a level
+# Fulfill quests and unlock gear to enhance stats even more with an inventory to go with
+
+def main():
+    # main function, runs it all
+
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
